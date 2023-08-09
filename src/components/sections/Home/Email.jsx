97d332618dbbe173866fe1fd3680e127040b931d
Bryan Petko <bapetko1@gmail.com>
2023-08-09 17:56:10 -0400

    Activated the email section

--- src/components/sections/Home/Email.jsx
+++ src/components/sections/Home/Email.jsx
@@ -1,22 +1,32 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
 import { Button, Card, Form, FormLabel, InputGroup } from 'react-bootstrap'
 
 export function Email() {
   const form = useRef()
+  const msgElement = useRef()
+  const [message, setMessage] = useState('')
 
   function sendEmail(e) {
     e.preventDefault()
 
-    // emailjs.sendForm('service_mwjsfxo', 'template_d4yr0dj', form.current, '-S_sWUsJzJnjVwS4J').then(
-    //   function (response) {
-    //     console.log('Success!', response.status, response.text)
-    //   },
-    //   function (error) {
-    //     console.log('Failed...', error)
-    //   }
-    // )
+    emailjs.sendForm('service_mwjsfxo', 'template_d4yr0dj', form.current, '-S_sWUsJzJnjVwS4J').then(
+      function (response) {
+        setMessage('Your email has been successfully sent')
+        msgElement.current.className = 'text-success'
+      },
+      function (error) {
+        setMessage(error)
+        msgElement.current.className = 'text-danger'
+      }
+    )
+
     form.current.reset()
+  }
+  function resetMessage() {
+    setMessage('')
+    if (msgElement.current.classList.contains('text-success')) msgElement.current.classList.toggle('text-success')
+    if (msgElement.current.classList.contains('text-danger')) msgElement.current.classList.toggle('text-danger')
   }
 
   return (
@@ -24,7 +34,7 @@ export function Email() {
       <Card className="w-100" style={{ height: 'fit-content', fontSize: '1rem' }}>
         <Card.Title className="ps-2">Ask me about my campaign</Card.Title>
         <Card.Body className="p-0 pt-1 ps-2 pb-1 pe-2">
-          <Form ref={form} onSubmit={sendEmail}>
+          <Form ref={form} onChange={resetMessage} onSubmit={sendEmail}>
             <Form.Group controlId="user-name">
               <Form.Control name="user_name" type="text" placeholder="Your Name" />
             </Form.Group>
@@ -34,9 +44,14 @@ export function Email() {
             <Form.Group controlId="email-content" className="mt-1">
               <Form.Control name="message" as="textarea" rows={2} placeholder="Enter your question here!" />
             </Form.Group>
-            <Button size="sm" type="submit" value="Send" className="mt-1">
-              Submit
-            </Button>
+            <div className="d-flex">
+              <Button size="sm" type="submit" value="Send" className="mt-1">
+                Submit
+              </Button>
+              <div className="m-2" ref={msgElement}>
+                {message}
+              </div>
+            </div>
           </Form>
         </Card.Body>
       </Card>
